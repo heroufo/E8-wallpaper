c0221d5453d77fc8fa64bd8b3b5a758307f81685
EveryMaker <heroufo@163.com>
2025-05-19 16:19:03 +0800

    Add files via upload

---
+++ wallpapers.js
@@ -0,0 +1,13 @@
+const wallpapers = [
+  { thumbnail: 'https://picsum.photos/id/1011/400/300', download: 'https://picsum.photos/id/1011/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1012/400/300', download: 'https://picsum.photos/id/1012/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1015/400/300', download: 'https://picsum.photos/id/1015/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1016/400/300', download: 'https://picsum.photos/id/1016/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1018/400/300', download: 'https://picsum.photos/id/1018/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1020/400/300', download: 'https://picsum.photos/id/1020/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1021/400/300', download: 'https://picsum.photos/id/1021/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1022/400/300', download: 'https://picsum.photos/id/1022/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1023/400/300', download: 'https://picsum.photos/id/1023/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1024/400/300', download: 'https://picsum.photos/id/1024/1920/1080' },
+  { thumbnail: 'https://picsum.photos/id/1025/400/300', download: 'https://picsum.photos/id/1025/1920/1080' },
+];
