52ca709615932e44770c9440d81053c210a88b84
EveryMaker <heroufo@163.com>
2025-05-19 16:39:41 +0800

    Update wallpapers.js

--- wallpapers.js
+++ wallpapers.js
@@ -1,7 +1,7 @@
 const wallpapers = [
   { thumbnail: 'https://www.helloimg.com/i/2025/05/19/682ae002b3ead.jpg', download: 'https://www.helloimg.com/i/2025/05/19/682add8f257a1.jpeg' },
   { thumbnail: 'https://www.helloimg.com/i/2025/05/19/682ae89e37812.jpg', download: 'https://www.helloimg.com/i/2025/05/19/682ae89eb70a3.jpeg' },
-  { thumbnail: 'https://www.helloimg.com/i/2025/05/19/682ae8a17e350.jpg', download: 'https://www.helloimg.com/i/2025/05/19/682ae8a441650.jpeg' },
+  { thumbnail: 'https://www.helloimg.com/i/2025/05/19/682aebbe2f275.jpg', download: 'https://www.helloimg.com/i/2025/05/19/682ae8a441650.jpeg' },
   { thumbnail: 'https://picsum.photos/id/1016/400/300', download: 'https://picsum.photos/id/1016/1920/1080' },
   { thumbnail: 'https://picsum.photos/id/1018/400/300', download: 'https://picsum.photos/id/1018/1920/1080' },
   { thumbnail: 'https://picsum.photos/id/1020/400/300', download: 'https://picsum.photos/id/1020/1920/1080' },
